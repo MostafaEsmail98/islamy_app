eb5e12c386cdba682effd9a12df3e482dc5cd307
mostafa esmail <mostafaelshrbeeny98@gmail.com>
2024-08-02 21:56:29 +0300

    finshed sebha tab

--- lib/features/home_page/presentation/views/widgets/custom_sebha_tab.dart
+++ lib/features/home_page/presentation/views/widgets/custom_sebha_tab.dart
@@ -4,75 +4,103 @@ import 'package:islamy_app/core/utils/styles.dart';
 import 'package:islamy_app/core/utils/title.dart';
 import 'package:islamy_app/core/utils/urlOfImage.dart';
 
-class CustomSebhaTab extends StatelessWidget {
+class CustomSebhaTab extends StatefulWidget {
   const CustomSebhaTab({super.key});
+
+  @override
+  State<CustomSebhaTab> createState() => _CustomSebhaTabState();
+}
+
+class _CustomSebhaTabState extends State<CustomSebhaTab> {
+  int counter = 0;
+  int i = 0;
+  List<String> tsbeeh = [
+    "سبحان الله",
+    "الله اكبر",
+    "لا اله الا الله",
+    "استغفر الله",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const TitleApp(),
-          SizedBox(
-            height: 100,
-            child: Image.asset(UrlOfImage.headSebhaImage),
-          ),
-          SizedBox(
-            height: 227,
-            child: Image.asset(UrlOfImage.bodySebhaImage),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "عدد التسبيحات",
-                style: Styles.textStyle25.copyWith(
-                  fontWeight: FontWeight.normal,
-                ),
-              )
-            ],
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: KColors.kPrimaryColor,
-                borderRadius: const BorderRadius.all(Radius.circular(15))),
-            height: 60,
-            width: 50,
-            child: const Center(
-                child: Text(
-              "1",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            )),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 100),
-            child: Container(
+      child: InkWell(
+        onTap: () {
+          counter++;
+          if (counter == 34) {
+            counter = 0;
+            i++;
+            if (i == tsbeeh.length) {
+              i = 0;
+            }
+          }
+
+          setState(() {});
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const TitleApp(),
+            SizedBox(
+              height: 100,
+              child: Image.asset(UrlOfImage.headSebhaImage),
+            ),
+            SizedBox(
+              height: 227,
+              child: Image.asset(UrlOfImage.bodySebhaImage),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "عدد التسبيحات",
+                  style: Styles.textStyle25.copyWith(
+                    fontWeight: FontWeight.normal,
+                  ),
+                )
+              ],
+            ),
+            Container(
               decoration: BoxDecoration(
                   color: KColors.kPrimaryColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(30))),
-              height: 50,
-              child: const Center(
-                child: Text(
-                  "سبحان الله",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold),
+                  borderRadius: const BorderRadius.all(Radius.circular(15))),
+              height: 60,
+              width: 50,
+              child:  Center(
+                  child: Text(
+                    "$counter",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: KColors.kPrimaryColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(30))),
+                height: 50,
+                child:  Center(
+                  child: Text(
+                    "${tsbeeh[i]}",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
